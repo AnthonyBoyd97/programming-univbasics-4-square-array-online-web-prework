0ae2a64750fe7cf60bf14960032bf6542ae2d6e0
Anthony N Boyd <anboyd97@gmail.com>
2019-09-04 09:23:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   new_array=Array.new
   while array[counter] do
-    new_array[counter]<<(array[counter]*array[counter])
+    new_array<<(array[counter]*array[counter])
     counter+=1
   end
   return new_array

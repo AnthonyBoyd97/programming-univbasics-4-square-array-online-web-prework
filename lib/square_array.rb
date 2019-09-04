18d1e6382154a197eb17df7f2e13e39cd8ba905c
Anthony N Boyd <anboyd97@gmail.com>
2019-09-04 09:18:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   counter = 0
+  new_array[]
   while array[counter] do
-    array[counter]=(array[counter]**2)
+    new_array[counter]=(array[counter]**2)
     counter+=1
   end
+  return new_array
 end
